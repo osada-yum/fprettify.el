b42e36eaa5dcbfa28de0a33467527bee69756fb6
osada-yum <osada.yum@gmail.com>
2021-07-25 19:57:50 +0900

    Renew. Add plusminus, select type.
    modified:   mechakucha.f90

--- mechakucha.f90
+++ mechakucha.f90
@@ -1,12 +1,28 @@
+# define are 3490
 program   test
-  implicit none
+  implicit     none
      integer    ::               i
+
    print   *  ,   "hi"
 i=   9
    do   i   =               1    ,9
       if    (i.le.    4  )      then
-print       *  ,  i, "a                                                                                                                    hi"
+!print       *  ,  i, "a                                                                                                                    hi"
 write ( *, * )                                  i
    end if
       end do
+                        ! print?
+      print     *   ,   3    *    ( 1                   +2)    ,   .true.   .and.   .false..or..true.
+      print*,+1,-9428
+
+  contains
+
+
+     subroutine   nanika  (   arg )
+        integer  ,    intent(  in  )            ::     arg
+        select   type  (  arg   )
+             type   is  (  integer  )
+     print*,"hi"
+           end select
+     end     subroutine     nanika
 end   program   test
